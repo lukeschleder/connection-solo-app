8196dfb7bdfbfa7bb9ddefb6f66d2eda38f9e01d
Luke Schleder <luke.schleder@gmail.com>
2019-12-13 16:42:36 -0600

    update app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,6 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
 import FormOne from '../FormOne/FormOne';
 import FormTwo from '../FormTwo/FormTwo';
 import FormThree from '../FormThree/FormThree';
@@ -57,13 +56,6 @@ class App extends Component {
               exact
               path="/home"
               component={UserPage}
-            />
-            {/* This works the same as the other protected route, except that if the user is logged in,
-            they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
             />
             <ProtectedRoute
               exact
